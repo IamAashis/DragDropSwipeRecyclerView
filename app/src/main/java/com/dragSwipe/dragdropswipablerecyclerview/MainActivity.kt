31ddbe306e3aead93811dc6f377fcc89056f4e4a
Aashis <thapaaashis20@gmail.com>
2023-01-18 08:28:02 +0545

    Addded adapter

--- app/src/main/java/com/dragSwipe/dragdropswipablerecyclerview/MainActivity.kt
+++ app/src/main/java/com/dragSwipe/dragdropswipablerecyclerview/MainActivity.kt
@@ -2,10 +2,135 @@ package com.dragSwipe.dragdropswipablerecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.dragSwipe.dragdropswipablerecyclerview.databinding.ActivityMainBinding
+import com.ernestoyaquello.dragdropswiperecyclerview.DragDropSwipeRecyclerView
+import com.ernestoyaquello.dragdropswiperecyclerview.listener.OnItemDragListener
+import com.ernestoyaquello.dragdropswiperecyclerview.listener.OnItemSwipeListener
+import com.ernestoyaquello.dragdropswiperecyclerview.listener.OnListScrollListener
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var dragDropAdapter: DragDropAdapter
+    private var itemsList = mutableListOf<String>()
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        createFakeItems()
+        setUpRecycler()
+    }
+
+    private fun createFakeItems() {
+        for (i in 0..25) {
+            itemsList.add("item $i")
+        }
+    }
+
+    private fun setUpRecycler() {
+        dragDropAdapter =
+            DragDropAdapter(itemsList)
+        val mList: DragDropSwipeRecyclerView = findViewById(R.id.list)
+        mList.layoutManager = LinearLayoutManager(this)
+        mList.adapter = dragDropAdapter
+
+        mList.orientation = DragDropSwipeRecyclerView.ListOrientation.VERTICAL_LIST_WITH_VERTICAL_DRAGGING
+        mList.reduceItemAlphaOnSwiping = true
+
+        //mList.disableSwipeDirection(DragDropSwipeRecyclerView.ListOrientation.DirectionFlag.RIGHT)
+
+        val onItemSwipeListener = object : OnItemSwipeListener<String> {
+            override fun onItemSwiped(
+                position: Int,
+                direction: OnItemSwipeListener.SwipeDirection,
+                item: String
+            ): Boolean {
+                Log.d("Main", "Position = $position, Direction = $direction, Item = $item")
+
+                when (direction) {
+                    //Delete Item
+                    OnItemSwipeListener.SwipeDirection.RIGHT_TO_LEFT -> {
+                        Toast.makeText(
+                            applicationContext,
+                            "$item deleted",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        //todo: add deleted code
+                    }
+                    //Archive Item
+                    OnItemSwipeListener.SwipeDirection.LEFT_TO_RIGHT -> {
+                        Toast.makeText(
+                            applicationContext,
+                            "$item archived",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        //todo: add archived code here
+                    }
+                    else -> return false
+                }
+                return false
+            }
+        }
+
+         val onItemDragListener = object : OnItemDragListener<String> {
+            override fun onItemDragged(previousPosition: Int, newPosition: Int, item: String) {
+                // Handle action of item being dragged from one position to another
+            }
+
+            override fun onItemDropped(initialPosition: Int, finalPosition: Int, item: String) {
+                // Handle action of item dropped
+            }
+        }
+
+        val onListScrollListener = object : OnListScrollListener {
+            override fun onListScrollStateChanged(scrollState: OnListScrollListener.ScrollState) {
+                // Handle change on list scroll state
+            }
+
+            override fun onListScrolled(scrollDirection: OnListScrollListener.ScrollDirection, distance: Int) {
+                // Handle scrolling
+            }
+        }
+        mList.swipeListener  = onItemSwipeListener
+
+        // button
+        fabAddItem()
+    }
+
+    private fun fabAddItem() {
+        binding.fabAdd.setOnClickListener {
+            Log.d("Main", "Button pressed")
+            val builder = AlertDialog.Builder(this)
+            val inflater = layoutInflater
+            val dialogLayout = inflater.inflate(R.layout.edit_text_layout, null)
+            val editText = dialogLayout.findViewById<EditText>(R.id.et_editText)
+
+            with(builder) {
+                setTitle("Add a note!")
+                setPositiveButton("OK") { dialog, which ->
+                    dragDropAdapter.updateItem(editText.text.toString())
+
+                    //todo add list update code
+
+                    Toast.makeText(
+                        applicationContext,
+                        "Text added successfully",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                setNegativeButton("Cancel") { dialog, which ->
+                    Log.d("Main", "Negative button clicked")
+                }
+                setView(dialogLayout)
+                show()
+            }
+        }
     }
 }
